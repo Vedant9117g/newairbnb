a184392624898e4e81bb75463bd15d7e5bf611e1
Vedant9117g <vedantw9117@gmail.com>
2025-01-14 22:18:38 +0530

    phase 1 (part c) error handling done.

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const ejsMate = require('ejs-mate');
 const port = 8080;
 const MONGO_URL = 'mongodb://127.0.0.1:27017/newairbnb';
 const methodOverride = require('method-override');
+const wrapAsync = require('./utils/wrapAsync');
+const ExpressError = require('./utils/ExpressError');
+const listingSchema = require('./utils/schema');
+
 // Connect to MongoDB
 mongoose
     .connect(MONGO_URL)
@@ -23,14 +27,26 @@ app.engine('ejs',ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateListing = (req, res, next) => {
+    let result = listingSchema.listingsSchema.validate(req.body);
+    console.log(result);
+
+    if (result.error) {
+        let error = result.error.details.map(err => err.message).join(',');
+        throw new ExpressError(400, error);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.get('/listings', async (req, res) => {
+app.get('/listings',validateListing, wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 // New listing form
 app.get('/listings/new', (req, res) => {
@@ -38,39 +54,45 @@ app.get('/listings/new', (req, res) => {
 });
 
 // Show form to create new listing
-app.get('/listings/:id', async (req, res) => {
+app.get('/listings/:id',validateListing, wrapAsync(async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-});
+}));
 
 // Create new listing
-app.post('/listings', async (req, res) => {
+app.post('/listings',validateListing, wrapAsync (async (req, res,next ) => {
+    // if(!req.body.listing) {
+    //     throw new ExpressError(400, 'Invalid Listing Data , please send a valid listing data');
+    // };
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 // Edit listing form
-app.get('/listings/:id/edit', async (req, res) => {
+app.get('/listings/:id/edit',validateListing,wrapAsync( async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
 /// Update listing
-app.put('/listings/:id', async (req, res) => {
+app.put('/listings/:id', validateListing ,wrapAsync(async (req, res) => {
+    if(!req.body.listing) {
+        throw new ExpressError(400, 'Invalid Listing Data , please send a valid listing data');
+    };
     const { id } = req.params;
     const updatedListing = await Listing.findByIdAndUpdate(id, req.body.listing, { new: true });
     res.redirect(`/listings/${updatedListing._id}`);
-});
+}));
 
 // Delete listing
-app.delete('/listings/:id', async (req, res) => {
+app.delete('/listings/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect('/listings');
-});
+}));
 
 
 // app.get('/test', async (req, res) => {
@@ -85,6 +107,18 @@ app.delete('/listings/:id', async (req, res) => {
 //   console.log('Sample listing saved');
 //   res.send('Sample listing success');
 // });
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError(404,'Page Not Found'));
+});
+
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).render('listings/error', { statusCode, message });
+});
+
 
 // Start the server
 app.listen(port, () => {
